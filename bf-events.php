1a1802b1af82a13522f367b9fd3139ab31d37ec4
whaitey <nincsilyenxx@gmail.com>
2025-08-01 12:39:45 +0200

    v1.2.1: Update banner background image and fix speaker permalinks

--- bf-events.php
+++ bf-events.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: BF Events
  * Description: Teljes körű eseménykezelő rendszer WordPress plugin
- * Version: 1.2.0
+ * Version: 1.2.1
  * Author: ZeusWeb
  * Plugin URI: https://github.com/whaitey/bf-events
  * GitHub Plugin URI: https://github.com/whaitey/bf-events
