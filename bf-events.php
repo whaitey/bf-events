cc66bc15093eda54e9bb42de562104460e329c41
Alex Kiss <alexkiss@Alexs-MacBook-Air.local>
2025-09-05 15:00:02 +0200

    chore(release): bump version to 1.4.10

--- bf-events.php
+++ bf-events.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: BF Events
  * Description: Teljes körű eseménykezelő rendszer WordPress plugin
- * Version: 1.4.9
+ * Version: 1.4.10
  * Author: ZeusWeb
  * Plugin URI: https://github.com/whaitey/bf-events
  * GitHub Plugin URI: https://github.com/whaitey/bf-events
